19d40c0a16b9c2d2f73e32f560f1173799d3f91e
flw <879465780@qq>
2020-10-20 23:39:54 +0800

    init

--- mybatisdemo/src/main/java/bean/Blog.java
+++ mybatisdemo/src/main/java/bean/Blog.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public  class Blog implements java.io.Serializable {
 
-    private int id;
+     private int id;
     private String title;
     private User author;
     private String body;
